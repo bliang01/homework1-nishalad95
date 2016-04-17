85d3d792e19b229eb1cd68fba932dddbbf1e5257
Nisha Lad <nishalad95@gmail.com>
2016-04-17 09:47:08 +0000

    changes to gauss-seidal

--- homework1/exercise3.py
+++ homework1/exercise3.py
@@ -60,7 +60,7 @@ def is_sdd(A):
  
     Returns
     -------
-    boolean: True if matrix A is strictly diagonally dominant, false otherwise
+    boolean statement: True if matrix A is strictly diagonally dominant, false otherwise
       
     """
     off_diag_elements = 0;
@@ -81,11 +81,24 @@ def jacobi_step(D, L, U, b, xk):
     
     Parameters
     ----------
-    
+    D : numpy.ndarray of ints or floats
+        The 2 dimensional n x n square matrix consisting 
+        of the leading diagonal elements of the matrix A
+    L : numpy.ndarray of ints or floats
+        The 2 dimensional n x n square matrix consisting 
+        of the elements above the leading diagonal in matrix A
+    U : numpy.ndarray of ints or floats
+        The 2 dimensional n x n square matrix consisting 
+        of the elements below the leading diagonal in matrix A
+    b : numpy.array of ints or floats
+        1D vector solution of Ax = b in the original problem
+    xk : numpy.array of ints or floats
+         1D guess vector, previous iteration
  
     Returns
     -------
-    xk1
+    xk1 : numpy.array
+          1D array as next iteration in the jacobi method
       
     """
     A = D + L + U
@@ -95,7 +108,9 @@ def jacobi_step(D, L, U, b, xk):
     # solve S(x(k+1)) = b - Tx(k)
     # S is diagonal matrix; inverse of S is the reciprocal of all leading diagonal elements
     T = L + U
+    xk = xk.astype(float)
     xk = xk.transpose()
+    b = b.astype(float)  
     b = b.transpose()
     difference = b - dot(T, xk)
     for i in range(0, len(A), 1):
@@ -109,6 +124,24 @@ xk = array([5, 6])
 print jacobi_step(D, L, U, xk, xk)
 
 def jacobi_iteration(A, b, x0, epsilon=1e-8):
+    """Returns three 2D matrices in an array.
+    
+    Parameters
+    ----------
+    A : ndarray
+        2 dimensional n x n square matrix to be decomposed.
+    b : numpy.array of ints or floats
+        1D vector solution of Ax = b in the original problem
+    x0 : numpy.array of ints or floats
+         1D original guess vector
+    epsilon : float
+              roundoff error convergence threshold, default set to 1e-8
+ 
+    Returns
+    -------
+    xk1 : numpy.array
+          1D array solution to the equation using the jacobi method
+    """
     D, L, U = decompose(A)
     xk1 = jacobi_step(D, L, U, b, x0)
     while (norm(xk1 - x0, 2) < epsilon):
@@ -120,7 +153,38 @@ x0 = array([1, 2])
 print jacobi_iteration(A, xk, x0)
 
 def gauss_seidel_step(D, L, U, b, xk):
-    pass
+    """Returns the next iteration xk1, given previous iteration xk, 
+    using the gauss-seidal iteration technique to solve linear equations numerically.
+    
+    Parameters
+    ----------
+    D : numpy.ndarray of ints or floats
+        The 2 dimensional n x n square matrix consisting 
+        of the leading diagonal elements of the matrix A
+    L : numpy.ndarray of ints or floats
+        The 2 dimensional n x n square matrix consisting 
+        of the elements above the leading diagonal in matrix A
+    U : numpy.ndarray of ints or floats
+        The 2 dimensional n x n square matrix consisting 
+        of the elements below the leading diagonal in matrix A
+    b : numpy.array of ints or floats
+        1D vector solution of Ax = b in the original problem
+    xk : numpy.array of ints or floats
+         1D guess vector, previous iteration
+ 
+    Returns
+    -------
+    xk1 : numpy.array
+          1D array as next iteration in the gauss-seidal method
+      
+    """
+    b = b.transpose()
+    xk = xk.tranpose()
+    difference = b - dot(L, xk)
+    S = D + U
+    xk1 = solve_triangular(S, difference)
+    xk1 = xk1.transpose()
+    return xk1
 
 def gauss_seidel_iteration(A, b, x0, epsilon=1e-8):
     pass
