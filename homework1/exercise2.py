c5879547ab1da02fec55db8f789ef428b95ff13b
Nisha Lad <nishalad95@gmail.com>
2016-04-22 03:57:44 +0000

    changes to exercise 2

--- homework1/exercise2.py
+++ homework1/exercise2.py
@@ -75,21 +75,4 @@ def gradient_descent(f, df, x, sigma=0.5, epsilon=1e-8):
     else:
 	return gradient_descent(f, df, x_k1 + 0.01)	
 
-#f = lambda x : x**2
-#df = lambda x : 2*x
-#print gradient_descent(f, df, 0.5)
-#print gradient_descent(f, df, 0)
-
-#f = lambda x : 0.25*x**4 - 0.5*x**2
-#df = lambda x : x**3 - x
-#print gradient_descent(f, df, 0)
-
-#f = lambda x : x**3
-#df = lambda x : 3*x**2
-#print gradient_descent(f, df, 0)
-
-#f = lambda x : -x**2
-#df = lambda x : -2*x
-#print gradient_descent(f, df, 0)
-
 
